aec5c615c8b61a2c94deeb7b5133946763fbe0ad
aloong <574202393@qq.com>
2018-12-19 10:08:11 +0800

    4显示天气信息

--- app/src/main/java/com/example/admin/lamiweather/util/HttpUtil.java
+++ app/src/main/java/com/example/admin/lamiweather/util/HttpUtil.java
@@ -1,5 +1,12 @@
 package com.example.admin.lamiweather.util;
 
+import com.example.admin.lamiweather.gson.Weather;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
@@ -9,4 +16,5 @@ public class HttpUtil {
         Request request = new Request.Builder().url(address).build();
         client.newCall(request).enqueue(callback);
     }
+
 }
